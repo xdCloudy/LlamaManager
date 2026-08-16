8021a19ae64d8a8b15f8da1f7afb96898f92839b
Cloudy <52116030+xdCloudy@users.noreply.github.com>
2026-08-16 05:25:27 +0100

    feat: add native application entry point

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use llamamanager::app::{set_bootstrap, App, Bootstrap};
+use tracing_subscriber::EnvFilter;
+
+fn main() {
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_target(false)
+        .try_init();
+
+    let bootstrap = Bootstrap::initialize().map_err(|error| {
+        tracing::error!(%error, "application bootstrap failed");
+        error.to_string()
+    });
+    set_bootstrap(bootstrap);
+
+    dioxus::launch(App);
+}
