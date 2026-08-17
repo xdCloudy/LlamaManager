5e5176a0942782dfca1e3038de606f66729aaac4
Cloudy <52116030+xdCloudy@users.noreply.github.com>
2026-08-17 15:02:48 +0100

    ui: brand desktop shell as LlamaWave

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use dioxus::desktop::{Config, LogicalSize, WindowBuilder};
 use llamamanager::app::{App, Bootstrap, set_bootstrap};
 use tracing_subscriber::EnvFilter;
 
@@ -16,5 +17,16 @@ fn main() {
     });
     set_bootstrap(bootstrap);
 
-    dioxus::launch(App);
+    let window = WindowBuilder::new()
+        .with_title("LlamaWave")
+        .with_inner_size(LogicalSize::new(1440.0, 900.0))
+        .with_min_inner_size(LogicalSize::new(1100.0, 700.0));
+
+    let config = Config::new()
+        .with_window(window)
+        .with_menu(None)
+        .with_background_color((7, 3, 18, 255))
+        .with_disable_context_menu(true);
+
+    dioxus::LaunchBuilder::new().with_cfg(config).launch(App);
 }
