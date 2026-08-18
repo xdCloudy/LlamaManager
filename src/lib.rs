b07ee462833b77db4be0b94d09a1df6e12194367
Cloudy <52116030+xdCloudy@users.noreply.github.com>
2026-08-18 06:53:31 +0100

    feat(server): export console control module

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@ pub mod paths;
 pub mod persistence;
 pub mod profile_generator;
 pub mod server_command;
+pub mod server_console;
 pub mod server_logs;
 pub mod server_process;
 pub mod server_readiness;
